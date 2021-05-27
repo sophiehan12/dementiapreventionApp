5cf4d90c9b7618559c43feaefdd197da1e6b8b8e
yoooungsun <dudtjs1021@hansung.ac.kr>
2021-05-27 23:39:10 +0900

    치매안심센터 - 구글지도 추가

--- app/src/main/java/com/example/dementiaprevention/HospitalActivity.java
+++ app/src/main/java/com/example/dementiaprevention/HospitalActivity.java
@@ -4,11 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class HospitalActivity extends AppCompatActivity {
+
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hospital);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this::onMapReady);
+    }
+
+
+    public void onMapReady(final GoogleMap gMap) {
+        googleMap = gMap;
+        LatLng SEOUL = new LatLng(37.56, 126.97);
+
+        //마커 설정
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(SEOUL);
+        markerOptions.title("서울");
+        markerOptions.snippet("한국의 수도");
+        googleMap.addMarker(markerOptions);
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10));
+
     }
 }
